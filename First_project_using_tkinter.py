cfa6e6ee0515045af31ef6e51769e6c51b3e573e
alonb4040 <97230740+alonb4040@users.noreply.github.com>
2022-01-12 00:03:16 +0200

    Update First_project_using_tkinter.py

--- First_project_using_tkinter.py
+++ First_project_using_tkinter.py
@@ -1,12 +1,19 @@
 
 '''
-News engine search.
 
-Sending customized articles details to an e-mail address. You can get articles based on the keywords that interest you. The engine will search the entire bank of articles, for only those whose titles are relevant to your keywords. You can filter by a full match or a partial match and even by a specific date.
+News search engine
 
-*Note - the script is insensitive (doesn't matter if you use big\small letters).
+Sending customized articles’ details to your Email address.
+You can get articles based on keywords for your interest. The engine will search the entire articles’ bank, by titles, based on what’s relevant to your keywords. You can filter the web searches results, by a full match or a partial match and even by a specific date.
 
-Partial match - searching for articles and sending all the articles that their title contains the sequence of the keyword. For example: for the keyword 'cov' the engine returns articles which in their title there is the sequence 'cov' such as: 'covid', 'covid-19' but for the keyword 'alon' the engine can return articles with the word 'alone' in their title. For these scenarios there is the full match option, so the engine will return articles with the exact sequence only.
+
+*Note - the script is insensitive (doesn't matter if you use upper-case\lower-case letters).
+
+
+Partial match - searching for articles and sending all the articles that their title contains the sequence of the keyword. 
+For example: for the keyword 'cov' the engine returns articles which in their title there׳s the sequence 'cov' such as: 'covid', 'covid-19' 
+but for the keyword 'alon' the engine can return articles with the word 'alone' in their title. For these scenarios there’s the full match option, so the engine will return articles with the exact sequence only.
+
 
 ENJOY:)
 '''
