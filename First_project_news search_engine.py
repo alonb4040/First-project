858b745acb3bf79a44b9ca0acc8a977422a02b64
alonb4040 <97230740+alonb4040@users.noreply.github.com>
2022-01-11 23:37:22 +0200

    Update First_project_news search_engine.py

--- First_project_news search_engine.py
+++ First_project_news search_engine.py
@@ -1,18 +1,15 @@
 
 '''
-###News engine search. Sending to e-mail customized articles details in a convenient and accessible way. You can get articles based on the keywords that interest you. The engine will search the entire bank of articles for only those whose titles are relevant to the keywords. You can search by full match or partial match.
+News engine search.
 
-*note - the script is insensitive (doesn't matter if you use big\small letters.
+Sending customized articles details to an e-mail address. You can get articles based on the keywords that interest you. The engine will search the entire bank of articles, for only those whose titles are relevant to your keywords. You can filter by a full match or a partial match and even by a specific date.
 
-Example of partial match. For the keyword 'cov' the engine returns articles which in their titles there are word/s that contain the sequence 'cov' such as: 'covid', 'covid-19'
+*Note - the script is insensitive (doesn't matter if you use big\small letters).
 
-*Note, sometimes by using full match choice only, you will get unwanted results for example. for the keyword 'Alon' you will get also results if the title contains the word 'alone' and for that you have the full match chioce only.
-
-You can choose articles from a specipic date or from anytime.
+Partial match - searching for articles and sending all the articles that their title contains the sequence of the keyword. For example: for the keyword 'cov' the engine returns articles which in their title there is the sequence 'cov' such as: 'covid', 'covid-19' but for the keyword 'alon' the engine can return articles with the word 'alone' in their title. For these scenarios there is the full match option, so the engine will return articles with the exact sequence only.
 
 ENJOY:)
 '''
-
 
 import ast
 import requests
