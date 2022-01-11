aed749e60c16346dbab320b1a681233cfbc752af
alonb4040 <97230740+alonb4040@users.noreply.github.com>
2022-01-11 23:37:42 +0200

    Update First_project_using_tkinter.py

--- First_project_using_tkinter.py
+++ First_project_using_tkinter.py
@@ -1,3 +1,17 @@
+
+'''
+News engine search.
+
+Sending customized articles details to an e-mail address. You can get articles based on the keywords that interest you. The engine will search the entire bank of articles, for only those whose titles are relevant to your keywords. You can filter by a full match or a partial match and even by a specific date.
+
+*Note - the script is insensitive (doesn't matter if you use big\small letters).
+
+Partial match - searching for articles and sending all the articles that their title contains the sequence of the keyword. For example: for the keyword 'cov' the engine returns articles which in their title there is the sequence 'cov' such as: 'covid', 'covid-19' but for the keyword 'alon' the engine can return articles with the word 'alone' in their title. For these scenarios there is the full match option, so the engine will return articles with the exact sequence only.
+
+ENJOY:)
+'''
+
+
 import tkinter as tk
 import ast
 import requests
